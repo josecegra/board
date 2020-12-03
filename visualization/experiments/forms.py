c821ce7d33b44d4942079ff488d9d00204c87051
josecegra <josecegra@gmail.com>
2020-12-03 20:41:30 +0100

    added bootstrap to forms

--- visualization/experiments/forms.py
+++ visualization/experiments/forms.py
@@ -2,6 +2,13 @@ from django import forms
 
 from .models import ExperimentModel
 class ExperimentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        # first call parent's constructor
+        super(ExperimentForm, self).__init__(*args, **kwargs)
+
+        self.fields['is_public'].required = False
+        self.fields['name'].widget.attrs.update({'class' : 'short-width'})
+
     class Meta:
         model = ExperimentModel
         fields = [ 'name','is_public']
--- visualization/torch_model_module/forms.py
+++ visualization/torch_model_module/forms.py
@@ -2,6 +2,14 @@ from django import forms
 
 from .models import TorchModel
 class TorchModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        # first call parent's constructor
+        super(TorchModelForm, self).__init__(*args, **kwargs)
+        self.fields['is_public'].required = False
+
+        self.fields['name'].widget.attrs.update({'class' : 'short-width'})
+        self.fields['problem_type'].widget.attrs.update({'class' : 'short-width'})
+
     class Meta:
         model = TorchModel
         fields = [ 'upload','name','problem_type','is_public']
